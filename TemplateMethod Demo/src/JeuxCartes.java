e1d4c4c3d590538afccc163a436f1b2a79555146
Playeurmania <alexandre.zirmi@depinfonancy.net>
2016-04-24 20:10:31 +0200

    Premier Update

--- TemplateMethod Demo/src/JeuxCartes.java
+++ TemplateMethod Demo/src/JeuxCartes.java
@@ -1,15 +1,37 @@
 
 public abstract class JeuxCartes {
 	
-	abstract void initialisation();
+	abstract void initialisation(int nbJoueurs);
 	abstract void débutJeu();
 	abstract void finJeu();
+	
+	public Carte[] Jeu;
+	
+	
+	public class Carte{
+		
+		private int valeur;
+		private String famille;
+		
+		private Carte(int val, String fam){
+			this.valeur = val;
+			this.famille = fam;
+		}
+		
+		public String toString(){
+			return this.valeur+" de "+this.famille;
+		}
+	}
 	
 	public final void jeu(int nbjoueurs){
 		
 	}
 	
 	public final void distribution(int nbjoueurs){
+		
+		/**
+		 * Création du jeu de cartes: 40 carte
+		 */
 		
 	}
 
