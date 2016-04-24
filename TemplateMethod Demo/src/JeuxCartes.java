2be3418133a90c757dcd29033057d4705262e3a7
Playeurmania <alexandre.zirmi@depinfonancy.net>
2016-04-24 22:03:58 +0200

    Cartes, Joueurs et distribution

--- TemplateMethod Demo/src/JeuxCartes.java
+++ TemplateMethod Demo/src/JeuxCartes.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public abstract class JeuxCartes {
 	
@@ -5,7 +6,9 @@ public abstract class JeuxCartes {
 	abstract void débutJeu();
 	abstract void finJeu();
 	
-	public Carte[] Jeu;
+	public int[] valeurs;
+	public String[] familles;
+	public Joueur[] joueurs;
 	
 	
 	public class Carte{
@@ -23,15 +26,68 @@ public abstract class JeuxCartes {
 		}
 	}
 	
+	public class Joueur{
+		private ArrayList<Carte> main;
+		private ArrayList<Carte> défausse;
+		private int numero;
+		
+		public Joueur(int i){
+			
+			this.main = new ArrayList<Carte>();
+			this.défausse = new ArrayList<Carte>();
+			this.numero=i;
+		}
+		
+		
+	}
+	
 	public final void jeu(int nbjoueurs){
 		
 	}
 	
-	public final void distribution(int nbjoueurs){
+	public final void distribution(){
 		
 		/**
-		 * Création du jeu de cartes: 40 carte
+		 * Création du jeu de cartes
 		 */
+		
+		System.out.println("Création des cartes");
+		Carte[] deck = new Carte[valeurs.length*familles.length];
+		int u = 0;
+		for (int i : valeurs){	
+			for(String j : familles){
+				deck[u]=new Carte(i,j);
+			}
+		}
+		System.out.println("Deck complet");
+		
+		/**
+		 * Mélange du deck
+		 */
+        for(int i=0;i<deck.length;i++){
+            
+        	Carte c;
+            int j = (int)Math.floor(Math.random()*(deck.length-i) + i);
+            c = deck[i];
+            deck[i] = deck[j];
+            deck[j] = c;
+        }
+        System.out.println("Deck mélangé");
+        
+        /**
+         * Distribution des cartes
+         */
+        for (Joueur j : joueurs){
+        	for (int i=0; i<(deck.length%joueurs.length); i++){
+        		j.main.add(deck[j.numero+i]);
+;        		
+        	}
+        }
+        
+        
+        
+		
+		
 		
 	}
 
