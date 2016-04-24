479a0fa0cdbd6ff9ec7038d912ddfb1901f444ca
Playeurmania <alexandre.zirmi@depinfonancy.net>
2016-04-24 19:40:12 +0200

    Classe mère et méthodes (structure)

--- TemplateMethod Demo/src/JeuxCartes.java
+++ TemplateMethod Demo/src/JeuxCartes.java
@@ -1,4 +1,17 @@
 
 public abstract class JeuxCartes {
+	
+	abstract void initialisation();
+	abstract void débutJeu();
+	abstract void finJeu();
+	
+	public final void jeu(int nbjoueurs){
+		
+	}
+	
+	public final void distribution(int nbjoueurs){
+		
+	}
+
 
 }
