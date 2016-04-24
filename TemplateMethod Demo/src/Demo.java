41e83dead6eb8cb24c1e92d328e7455d9b7af538
Florian-Bertot <florian.bertot@gmail.com>
2016-04-24 19:58:29 +0200

    Update Demo.java

--- TemplateMethod Demo/src/Demo.java
+++ TemplateMethod Demo/src/Demo.java
@@ -1,9 +1,17 @@
-
 public class Demo {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+   final void jouerUnePartie(int nombreDeJoueurs){
+	     initialize();
+	     int joueur = 0;
+	     boolean fini = false;
+	     while(!fini){
+	       startPlay(joueur);
+	       joueur = (joueur + 1) % nombreDeJoueurs;
+	     }
+	     proclamerLeVainqueur();
+	   }
+	
+	public static void main(String[] args){
+		jouerUnePartie(2);
 	}
-
 }
