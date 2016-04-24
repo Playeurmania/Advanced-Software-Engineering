ac7c9de972b89a416b1b154c0b9ef9892d4edf6b
Florian-Bertot <florian.bertot@gmail.com>
2016-04-24 22:48:31 +0200

    Update BatailleCorse.java

--- TemplateMethod Demo/src/BatailleCorse.java
+++ TemplateMethod Demo/src/BatailleCorse.java
@@ -2,38 +2,45 @@ import java.util.ArrayList;
 
 public class BatailleCorse extends JeuxCartes{
 	
-	public ArrayList<Carte> cartesJ1;
-	public ArrayList<Carte> cartesJ2;
-	int joueur;
+	public boolean fini = false;
 	
-	
-	void initialisation(int joueur){
+	void initialisation(int nbJoueurs){
 		System.out.println("La partie de bataille corse est initialisée !");
-		ArrayList<Carte> cartesJ1 = null;
-		ArrayList<Carte> cartesJ2 = null;
+		distribution();
 		
 	}
 	
-	void startPlay(int joueur){
-		System.out.println("Le joueur "+joueur+" joue !");
-		ArrayList<Carte> Current = null;
-		if (joueur == 0){
-			Carte c = cartesJ1.remove(0);
-			Current.add(c);
-			int taille = Current.size();
+	void startPlay(Joueur joueur, Joueur autreJoueur){
+		System.out.println("Le joueur "+joueur.numero+" joue !");
+			Carte c = joueur.main.remove(0);
+			System.out.println("Le joueur "+joueur.numero+" a joué la carte "+c.toString());
+			joueur.défausse.add(c);
+			int taille = joueur.défausse.size();
 			if (taille>0){
-				if (Current.get(taille) == Current.get(taille-1)){
-					cartesJ1.addAll(Current);
+				if (joueur.défausse.get(taille) == joueur.défausse.get(taille-1)){
+					joueur.main.addAll(joueur.défausse);
+					joueur.défausse.clear();
 				} 
-			}	
-		} 
+			}
+			if (joueur.main.size() == 0){
+				fini = true;
+			}
+		autreJoueur.défausse = joueur.défausse;
 	}
 	
 	void débutJeu(){
 		
-	}
+		Joueur joueur;
+		Joueur autreJoueur;
+	    	while(!fini){
+		      startPlay(joueur, autreJoueur);
+		      Joueur alter = joueur;
+		      joueur = autreJoueur;
+		      autreJoueur=alter;
+	     	}
+	   }
 	
 	void finJeu(){
-		
+		System.out.println("La partie est terminée !");
 	}
 }
