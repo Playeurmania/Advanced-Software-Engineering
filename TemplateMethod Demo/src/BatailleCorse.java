f89e7f024c4d66b3795fd384a03b5ee7bc13f2b8
Florian-Bertot <florian.bertot@gmail.com>
2016-04-24 19:54:17 +0200

    Update BatailleCorse.java

--- TemplateMethod Demo/src/BatailleCorse.java
+++ TemplateMethod Demo/src/BatailleCorse.java
@@ -1,4 +1,32 @@
-
-public class BatailleCorse {
-
+public class BatailleCorse extends JeuxCartes{
+	
+	public String[] cartesJ1;
+	public String[] cartesJ2;
+	int joueur;
+	
+	
+	void initialize(){
+		System.out.println("La partie de bataille corse est initialisée !");
+		String[] cartesJ1 = {"Pi:3", "Ca:4"};
+		String[] cartesJ2 = {"Pi:7", "Co:12"};
+		
+	}
+	
+	void startPlay(int joueur){
+		System.out.println("Le joueur "+joueur+" joue !");
+		String[] current;
+		if (joueur == 0){
+			//On retire la première carte du tas du joueur
+			//On l'ajoute à current
+			if (/*current[n] == current[n-1]*/true){
+				//On rajoute current au tas du J1
+			} 		
+		} else {
+			//On fait l'inverse
+		}
+	}
+	
+	void endplay(){
+		
+	}
 }
